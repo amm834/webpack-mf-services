dd05ab14c4471a3c153de9e499153c6a4622b8c6
amm834 <aungmyatmoe834@gmail.com>
2022-12-18 09:46:53 +0630

    feat: add router to the product detail app

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -4,14 +4,22 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import Header from 'home/Header';
 import Footer from 'home/Footer';
+import ProductDetail from "./ProductDetail";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 const App = () => (
-    <div className="mt-10 text-3xl mx-auto max-w-6xl">
-        <Header/>
-        <main className="my-8">
-            Product Detail Page
-        </main>
-        <Footer/>
-    </div>
+    <BrowserRouter>
+
+        <div className="mt-10 text-3xl mx-auto max-w-6xl">
+            <Header/>
+            <Routes>
+                <Route path="/product/:id" element={<ProductDetail/>}/>
+            </Routes>
+            <main className="my-8">
+            </main>
+
+            <Footer/>
+        </div>
+    </BrowserRouter>
 );
 ReactDOM.render(<App/>, document.getElementById("app"));
